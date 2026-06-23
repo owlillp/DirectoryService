bcc818a845f53f6612cf0955df1b268b10bb41d5
owlillp <d.georgeo.on@gmail.com>
2026-06-23 13:21:44 +0300

    Structure
    -добавил базовую структуру проекта

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Counter from "./counter";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <Counter />
+        <div>
+          <span>Directory Service</span>
+          <span>Тут будут разделы</span>
+        </div>
       </main>
     </div>
   );
